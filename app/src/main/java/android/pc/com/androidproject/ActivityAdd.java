c6a9d36ef1fe6f4a8e4a637dd79bfde47ee9763c
pauloschmidt <paulosergiofuno@gmail.com>
2019-01-23 15:20:00 -0800

    activity change and new ui

---
+++ app/src/main/java/android/pc/com/androidproject/ActivityAdd.java
@@ -0,0 +1,18 @@
+package android.pc.com.androidproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ActivityAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+
+}
--- app/src/main/java/android/pc/com/androidproject/MainActivity.java
+++ app/src/main/java/android/pc/com/androidproject/MainActivity.java
@@ -1,13 +1,30 @@
 package android.pc.com.androidproject;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_float);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityAdd();
+            }
+        });
+    }
+
+    public void openActivityAdd() {
+        Intent intent = new Intent(this, ActivityAdd.class);
+        startActivity(intent);
     }
 }
